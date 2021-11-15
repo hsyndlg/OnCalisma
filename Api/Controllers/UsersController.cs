2be53351410e0995662802218a8a64ac1fa2ff84
Huseyin Dalgic <hsyndlgc5334@hotmail.com>
2021-11-15 23:44:18 +0300

    allprojectupdate

--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -1,29 +1,66 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
+using Api.Model;
+using System.Data;
+using Dapper;
 
 namespace Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class UsersController : ControllerBase
     {
+        private readonly string Constr = "Server=HUSEYIN\\SQLEXPRESS;Database=OnCalisma;User Id=sa;Password=123456";
         [HttpGet]
         public IActionResult GetById(int id)
         {
-            return Ok();
+            string sql = $"select * from Users where Id={id}";
+            using (var connection = new SqlConnection(Constr))
+            {
+                var user = connection.Query<User>(sql,commandType: CommandType.Text).FirstOrDefault();
+                if (user!=null)
+                {
+                    return Ok(user);
+                }
+                else
+                {
+                    return BadRequest("başarısız işlem");
+                }
+            }
+            
         }
         [HttpGet]
         public IActionResult GetAll()
         {
             string sql = "select * from Users";
-            using (var connection = new SqlConnection())
+            using (var connection = new SqlConnection(Constr))
             {
-                 
+                var users = connection.Query<User>(sql, commandType: CommandType.Text).ToList();
+                if (users!=null)
+                {
+                    return Ok(users);
+                }
+                else
+                {
+                    return BadRequest("işlem başarısız");
+                }
             }
-            return Ok();
+            
+        }
+        [HttpPost]
+        public IActionResult Login(string userName,string password)
+        {
+            string sql = $"select * from Users where UserName='@username' and Password='@password'";
+            using (var connection = new SqlConnection(Constr))
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@username",userName,DbType.String);
+                parameters.Add("@password",password,DbType.String);
+                var result = connection.Query<User>(sql);
+                if (result != null) return Ok(result);
+                else return BadRequest("giriş başarısız");
+            }
         }
     }
 }
---
+++ Api/Model/User.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string SurName { get; set; }
+        
+        
+    }
+}
